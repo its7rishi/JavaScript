6f607d4f85166ee0772c3e85000370b1232f1a5d
Saptarshi Majumdar <saptarshi.maj@yahoo.com>
2022-01-25 00:32:27 +0530

    Corrected fetch error.
    Corrected the fetch error in the script.js file. All working now.

--- script.js
+++ script.js
@@ -2,17 +2,49 @@ const api = {
   url: 'http://api.openweathermap.org/data/2.5/',
   key: 'eca6453c758a17a632ceaf31557b0cf8',
 };
+// http://api.openweathermap.org/data/2.5/weather?q=mumbai&units=metric&APPID=eca6453c758a17a632ceaf31557b0cf8
+// Search
+let searchInput = document.getElementById('search-input');
+let submitSearch = document.querySelector('form');
+let weather;
+submitSearch.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let query = searchInput.value;
+  searchInput.value = '';
 
-fetch(`${api.url}weather?q=london&units=metric&APPID=${api.key}`)
-  .then((res) => res.json())
-  .then((data) => {
-    let weather = data;
-    console.log(weather.sys.country);
+  fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then((response) => response.json())
+    .then((result) => {
+      // console.log(result);
+      weather = result;
 
-    let location = document.querySelector('weather-location');
-    let p = document.createElement('p');
-    p.innerHTML = '<h1>Hello</h1>';
-  })
-  .catch((err) => {
-    throw new Error(err);
-  });
+      // Populate Location Data
+      let location = document.getElementById('weather-location');
+      location.innerHTML = weather.name + ', ' + weather.sys.country;
+
+      //   Populate Current Temperature
+      let currentTemp = document.getElementById('current-temp');
+      currentTemp.innerHTML = weather.main.temp + '&deg;' + '' + 'C';
+
+      //   Populate Feels Like Temperature
+
+      let feelsLike = document.getElementById('feels-like');
+      feelsLike.innerHTML = 'Feels Like: ' + weather.main.feels_like + '&deg;C';
+
+      // Populate Min Temperature
+      let minTemp = document.getElementById('min-temp');
+      minTemp.innerHTML = 'Min Temp: ' + weather.main.temp_min + '&deg;C';
+
+      // Populate Max Temperature
+      let maxTemp = document.getElementById('max-temp');
+      maxTemp.innerHTML = 'Max Temp: ' + weather.main.temp_max + '&deg;C';
+
+      // Populate Clouds Data
+      let cloud = document.getElementById('clouds');
+      cloud.innerHTML = weather.weather[0].main;
+
+      //   Populate Humidity data
+      let humidity = document.getElementById('humidity');
+      humidity.innerHTML = 'Humidity: ' + weather.main.humidity + '&percnt;';
+    });
+});
